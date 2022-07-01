c88dc0a0bb841ade193b329f721c4243c3071ce7
Heloriel <andradematheus.dev@gmail.com>
2022-07-01 08:58:59 -0300

    Fix: Post link, no data recived component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -16,24 +16,23 @@ export const Post = (props: IPostProps) => {
   });
 
   return (
-    <Link
-      to={props.slug}
-      className="flex flex-col w-full bg-white max-h=[259px] min-h-[163px] rounded shadow-post p-6 mb-12"
-    >
+    <div className="flex flex-col w-full bg-white max-h=[259px] min-h-[163px] rounded shadow-post p-6 mb-12">
       <div className="flex w-full flex-row justify-between mb-6">
         <span className="text-base text-zinc-500 font-lexend">{formatedDate}</span>
         <span>
           <Heart size={24} />
         </span>
       </div>
-      <div className="mb-2">
-        <h1 className="font-lexend text-2xl">{props.title}</h1>
-      </div>
-      <div>
-        <span className="flex text-lg text-zinc-500 font-lexend text-justify w-full">
-          {props.children}
-        </span>
-      </div>
-    </Link>
+      <Link to={props.slug}>
+        <div className="mb-2">
+          <h1 className="font-lexend text-2xl">{props.title}</h1>
+        </div>
+        <div>
+          <span className="flex text-lg text-zinc-500 font-lexend text-justify w-full">
+            {props.children}
+          </span>
+        </div>
+      </Link>
+    </div>
   );
 };
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -28,9 +28,19 @@ export const IndexPage = () => {
 
   const { data } = useQuery<{ codelandia01S: Post[] }>(GET_POSTS_QUERY);
 
+  if (!data) {
+    return (
+      <div className="flex flex-1 items-center justify-center">
+        <span className="text-gray-500 text-3xl italic">
+          "Quando eu cheguei aqui tudo era mato..."
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div className="w-2/3 pt-24">
-      {data?.codelandia01S.map((post) => {
+      {data.codelandia01S.map((post) => {
         return (
           <Post key={post.id} date={post.published} slug={post.slug} title={post.title}>
             {post.content.text}
