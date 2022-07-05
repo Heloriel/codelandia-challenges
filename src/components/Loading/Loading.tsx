041ac8012b748ebf765a4353842e404a0bdaf0a4
Heloriel Thrayn <helore.art@gmail.com>
2022-07-04 22:27:21 -0300

    Fix: Loading spinner overlay

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,7 +2,7 @@ import "./Loading.css";
 
 export const Loading = () => {
   return (
-    <div className="flex w-full h-screen items-center justify-center">
+    <div className="flex w-full h-screen items-center justify-center bg-white absolute top-0 left-0 bottom-0 right-0">
       <div className="loading-spinner"></div>
     </div>
   );
